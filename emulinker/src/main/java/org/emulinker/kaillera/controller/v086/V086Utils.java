6c9ef17c9f212b5e0249452376fc3cc173b029dd
Jonn <hopskipnfall@users.noreply.github.com>
2021-11-25 16:57:11 +0900

    Format!

--- emulinker/src/main/java/org/emulinker/kaillera/controller/v086/V086Utils.java
+++ emulinker/src/main/java/org/emulinker/kaillera/controller/v086/V086Utils.java
@@ -22,7 +22,7 @@ public final class V086Utils {
     byte[] data = new byte[len / 2];
     for (int i = 0; i < len; i += 2) {
       data[i / 2] =
-        (byte) ((Character.digit(s.charAt(i), 16) << 4) + Character.digit(s.charAt(i + 1), 16));
+          (byte) ((Character.digit(s.charAt(i), 16) << 4) + Character.digit(s.charAt(i + 1), 16));
     }
     return data;
   }
