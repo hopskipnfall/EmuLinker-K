65c52a31cc654bb4980e6abef18237bbc67d1a50
Jonn <hopskipnfall@users.noreply.github.com>
2022-06-26 03:18:18 +0900

    Remove manual threading logic and replace with coroutines and ktor

--- emulinker/src/main/java/org/emulinker/kaillera/pico/ServerMain.kt
+++ emulinker/src/main/java/org/emulinker/kaillera/pico/ServerMain.kt
@@ -50,59 +50,61 @@ fun main(): Unit =
     //        reporter.start(30, SECONDS)
     //      }
     //  // Hacky code but it works! Tests that two users can make and play a game.
-  //  // TODO(nue): Move this into a test file in a subsequent PR.
-  //  runBlocking {
-  //    delay(4.seconds)
-  //
-  //    arrayOf(
-  //        async {
-  //          EvalClient("testuser1", io.ktor.network.sockets.InetSocketAddress("127.0.0.1", 27888))
-  //              .use {
-  //                delay(5.seconds)
-  //
-  //                it.connectToDedicatedPort()
-  //                it.start()
-  //
-  //                delay(1.seconds)
-  //
-  //                it.createGame()
-  //
-  //                delay(5.seconds)
-  //
-  //                it.startOwnGame()
-  //
-  //                delay(30.seconds)
-  //                it.dropGame()
-  //                delay(1.seconds)
-  //                it.quitGame()
-  //                delay(1.seconds)
-  //                it.quitServer()
-  //
-  //                delay(15.seconds)
-  //              }
-  //        },
-  //        async {
-  //          EvalClient("testuser2", io.ktor.network.sockets.InetSocketAddress("127.0.0.1", 27888))
-  //              .use {
-  //                delay(9.seconds)
-  //
-  //                it.connectToDedicatedPort()
-  //                it.start()
-  //
-  //                delay(1.seconds)
-  //                it.joinAnyAvailableGame()
-  //
-  //                delay(40.seconds)
-  //                it.quitServer()
-  //              }
-  //        })
-  //        .forEach { it.join() }
-  //
-  //    logger.atInfo().log("Shutting down everything else")
-  //
-  //    component.accessManager.stop()
-  //    component.kailleraServerController.stop()
-  //    component.kailleraServer.stop()
-  //    component.masterListUpdater.stop()
-  //  }
-}
+    //  // TODO(nue): Move this into a test file in a subsequent PR.
+    //  runBlocking {
+    //    delay(4.seconds)
+    //
+    //    arrayOf(
+    //        async {
+    //          EvalClient("testuser1", io.ktor.network.sockets.InetSocketAddress("127.0.0.1",
+    // 27888))
+    //              .use {
+    //                delay(5.seconds)
+    //
+    //                it.connectToDedicatedPort()
+    //                it.start()
+    //
+    //                delay(1.seconds)
+    //
+    //                it.createGame()
+    //
+    //                delay(5.seconds)
+    //
+    //                it.startOwnGame()
+    //
+    //                delay(30.seconds)
+    //                it.dropGame()
+    //                delay(1.seconds)
+    //                it.quitGame()
+    //                delay(1.seconds)
+    //                it.quitServer()
+    //
+    //                delay(15.seconds)
+    //              }
+    //        },
+    //        async {
+    //          EvalClient("testuser2", io.ktor.network.sockets.InetSocketAddress("127.0.0.1",
+    // 27888))
+    //              .use {
+    //                delay(9.seconds)
+    //
+    //                it.connectToDedicatedPort()
+    //                it.start()
+    //
+    //                delay(1.seconds)
+    //                it.joinAnyAvailableGame()
+    //
+    //                delay(40.seconds)
+    //                it.quitServer()
+    //              }
+    //        })
+    //        .forEach { it.join() }
+    //
+    //    logger.atInfo().log("Shutting down everything else")
+    //
+    //    component.accessManager.stop()
+    //    component.kailleraServerController.stop()
+    //    component.kailleraServer.stop()
+    //    component.masterListUpdater.stop()
+    //  }
+    }
